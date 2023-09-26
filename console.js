d8e636fcef71c0a2508292ba4454a88a873e5beb
A-Bitz <118044372+A-Bitz@users.noreply.github.com>
2023-09-26 08:23:46 +0530

    Add files via upload

---
+++ console.js
@@ -0,0 +1,11 @@
+//hey this is just a comment
+
+console.log("ONE API HACKATHON ")
+console.log("2023 ")
+
+function main(){
+  return null;
+}
+
+console.log("Christ Deemed To Be University")
+console.log("LAVASA AND BANGLORE")
